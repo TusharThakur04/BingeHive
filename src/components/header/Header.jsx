97f36cb647da3e5d5281a87dbe7bafdc9b887d00
Tushar <80577646+TusharThakur04@users.noreply.github.com>
2025-01-23 13:52:09 +0530

    modified:   src/assets/icons.js 	modified:   src/assets/images.js 	modified:   src/components/header/Header.jsx 	new file:   src/components/header/Header.styled.js 	modified:   src/screens/home/HomeScreen.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import {
+  BrandWrapper,
+  HeaderIconWrapper,
+  HeaderWrapper,
+  NavWrapper,
+} from "./Header.styled";
+import { Container } from "../../styles/global/default";
+import { Images } from "../../assets/images";
+import { Icons } from "../../assets/icons";
 
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <HeaderWrapper>
+      <Container>
+        <div className="header-content flex items-centre justify-between">
+          <BrandWrapper>
+            <img src="" alt="hello" />
+          </BrandWrapper>
+          <NavWrapper>
+            <button>
+              <img src="" alt="hi" />
+            </button>
+          </NavWrapper>
+        </div>
+      </Container>
+    </HeaderWrapper>
+  );
 };
 
 export default Header;
---
+++ src/components/header/Header.styled.js
@@ -0,0 +1,6 @@
+import { styled } from "styled-components";
+import { Link } from "react-router-dom";
+export const HeaderWrapper = styled.header``;
+export const BrandWrapper = styled(Link)``;
+export const NavWrapper = styled.nav``;
+export const HeaderIconWrapper = styled.div``;
