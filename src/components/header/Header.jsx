6cdd4610ae8efd3658f0f4f34704b50844418df4
Tushar <80577646+TusharThakur04@users.noreply.github.com>
2025-01-23 23:31:50 +0530

    modified:   src/components/header/Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,15 +8,14 @@ import {
 import { Container } from "../../styles/global/default";
 import { Images } from "../../assets/images";
 import { Icons } from "../../assets/icons";
+import routeConstants from "../../constants/routeConstants";
 
 const Header = () => {
   return (
-    <HeaderWrapper>
-      <Container>
+    <HeaderWrapper className={`flex items-center`}>
+      <Container className="w-full">
         <div className="header-content flex items-centre justify-between">
-          <BrandWrapper>
-            <img src="" alt="hello" />
-          </BrandWrapper>
+          <BrandWrapper to={routeConstants.HOME}></BrandWrapper>
           <NavWrapper>
             <button>
               <img src="" alt="hi" />
