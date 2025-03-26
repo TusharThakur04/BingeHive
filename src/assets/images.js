5f01a665f0709cf95f7959322414dd011481446d
Tushar <80577646+TusharThakur04@users.noreply.github.com>
2025-03-27 02:24:00 +0530

    modified:   src/assets/images.js 	new file:   src/assets/images/genre_1.png 	new file:   src/assets/images/genre_2.png 	new file:   src/assets/images/genre_3.png 	new file:   src/assets/images/genre_4.png 	new file:   src/assets/images/genre_5.png

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,3 +1,8 @@
 import HomeBanner from "./images/home_banner.png";
+import Genre1 from "./images/genre_1.png";
+import Genre2 from "./images/genre_2.png";
+import Genre3 from "./images/genre_3.png";
+import Genre4 from "./images/genre_4.png";
+import Genre5 from "./images/genre_5.png";
 
-export const Images = { HomeBanner };
+export const Images = { HomeBanner, Genre1, Genre2, Genre3, Genre4, Genre5 };
