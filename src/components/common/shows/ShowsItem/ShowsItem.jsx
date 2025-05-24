6da9e2b86331bd01005d00600a91199636aab6c4
Tushar <80577646+TusharThakur04@users.noreply.github.com>
2025-05-24 23:58:08 +0530

    modified:   src/components/common/shows/ShowsItem/ShowsItem.jsx 	modified:   src/components/common/shows/ShowsItem/ShowsItem.styles.js

--- src/components/common/shows/ShowsItem/ShowsItem.jsx
+++ src/components/common/shows/ShowsItem/ShowsItem.jsx
@@ -1,5 +1,5 @@
 import { Icons } from "../../../../assets/icons";
-import routeConstants from "../../../../constants/errorConstants";
+import routeConstants from "../../../../constants/routeConstants";
 import {
   DEFAULT_SHOWS,
   HIGH_RATED_SHOWS,
@@ -10,7 +10,64 @@ import { ShowsItemWrapper } from "./ShowsItem.styles";
 import PropTypes from "prop-types";
 
 const ShowsItem = ({ itemData, itemType }) => {
-  return <ShowsItemWrapper></ShowsItemWrapper>;
+  return (
+    <ShowsItemWrapper to={routeConstants.SHOWS + `/${itemData?.id}`}>
+      <div className="item-content bg-black10">
+        <div className="item-img">
+          <img src={itemData?.image?.medium} alt="" />
+        </div>
+        <div className="item-body">
+          <div className="flex items-center justify-between flex-wrap item-info">
+            <div className="item-title font-semibold">
+              {itemData?.name?.length > 16
+                ? itemData?.name?.substring(0, 16) + "..."
+                : itemData?.name}
+            </div>
+
+            {itemType === HIGH_RATED_SHOWS && (
+              <div className="rating flex items-center">
+                <img className="rating-star" src={Icons.StarRed} alt="" />
+                <Text className="rating-value text-sm">
+                  {itemData?.rating?.average} / 10
+                </Text>
+              </div>
+            )}
+
+            {itemType === NEW_SHOWS && (
+              <div className="premiere flex items-center">
+                <Text className="text-sm">{itemData?.premiered}</Text>
+              </div>
+            )}
+
+            {itemType === DEFAULT_SHOWS && (
+              <>
+                <div className="rating flex items-center">
+                  <img className="rating-star" src={Icons.StarRed} alt="" />
+                  <Text className="rating-value text-sm">
+                    {itemData?.rating?.average || "NA"} / 10
+                  </Text>
+                </div>
+                <div className="genre">
+                  <div className="text-sm">
+                    <span className="font-medium">Genre :</span>{" "}
+                    <Text className="text-sm inline">
+                      {itemData?.genres?.length > 0 &&
+                        itemData?.genres?.join(", ")}
+                    </Text>
+                  </div>
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </ShowsItemWrapper>
+  );
 };
 
 export default ShowsItem;
+
+ShowsItem.propTypes = {
+  itemData: PropTypes.object.isRequired,
+  itemType: PropTypes.string,
+};
