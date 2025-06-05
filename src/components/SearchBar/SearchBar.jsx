a1fa2ddf5611b49afba1dc358277bd9f5d6919a2
Tushar <80577646+TusharThakur04@users.noreply.github.com>
2025-06-06 00:03:08 +0530

    new file:   src/components/SearchBar/SearchBar.jsx 	new file:   src/components/SearchBar/SearchBar.styles.js 	modified:   src/screens/search/SearchScreen.jsx

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,13 @@
+import { SearchBarWrapper } from "./SearchBar.styles";
+
+const SearchBar = () => {
+  return (
+    <SearchBarWrapper>
+      <div>
+        <div></div>
+      </div>
+    </SearchBarWrapper>
+  );
+};
+
+export default SearchBar;
--- src/screens/search/SearchScreen.jsx
+++ src/screens/search/SearchScreen.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import SearchBar from "../../components/SearchBar/SearchBar";
 
 const SearchScreen = () => {
-  return <div>SearchScreen</div>;
+  return (
+    <div className="top-spacing-fix">
+      <SearchBar></SearchBar>
+    </div>
+  );
 };
 
 export default SearchScreen;
