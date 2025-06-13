4db6bfc688ee30b5237e0005e61c853cd9e8f962
Tushar <80577646+TusharThakur04@users.noreply.github.com>
2025-06-13 10:10:16 +0530

    modified:   README.md 	modified:   src/components/shows/episodeItem/EpisodeItem.jsx

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Visit - [BingeHive](https://v0-new-project-aw9qofp1n7d-7rw0act8e-tusharthakur04s-projects.vercel.app)
+Visit - [BingeHive](https://v0-new-project-aw9qofp1n7d.vercel.app/)
--- src/components/shows/episodeItem/EpisodeItem.jsx
+++ src/components/shows/episodeItem/EpisodeItem.jsx
@@ -1,10 +1,35 @@
 import PropTypes from "prop-types";
 import { EpisodeItemWrapper } from "./EpisodeItem.styles";
+import { Link } from "react-router-dom";
 
 const EpisodeItem = ({ episodeData }) => {
   return (
     <EpisodeItemWrapper>
-      <div></div>
+      <div className="episode-num text-gray60 text-xl flex items-start">
+        {episodeData?.number < 10
+          ? "0" + episodeData.number
+          : episodeData.number}
+      </div>
+      <div className="episode-full grid">
+        <Link to={episodeData?.url} target="_blank" className="episode-img">
+          <img
+            src={episodeData?.image?.medium /*|| Images.NoImage*/}
+            alt=""
+            className="object-fit-cover"
+          />
+        </Link>
+        <div className="episode-body">
+          <div className="episode-head">
+            <h4 className="text-xl episode-title">{episodeData?.name}</h4>
+            <p
+              className="episode-summary text-gray60"
+              dangerouslySetInnerHTML={{
+                __html: episodeData?.summary,
+              }}
+            ></p>
+          </div>
+        </div>
+      </div>
     </EpisodeItemWrapper>
   );
 };
